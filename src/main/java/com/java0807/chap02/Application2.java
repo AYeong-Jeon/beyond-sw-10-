de30b4317bab36afbcc90149e7c4ad759827d31f
AYeong-Jeon <ay98923@naver.com>
2024-08-08 18:45:00 +0900

    0808

--- src/main/java/com/java0807/chap02/Application2.java
+++ src/main/java/com/java0807/chap02/Application2.java
@@ -1,9 +1,10 @@
 package src.main.java.com.java0807.chap02;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Application2 {
     public static void main(String[] args) throws IOException {
