dc3671611ddf75849cec1fb69733331f398a3029
AYeong-Jeon <ay98923@naver.com>
2024-08-09 11:35:26 +0900

    0808

---
+++ src/main/java/com/java0808/chap01/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package src.main.java.com.java0808.chap01;
+
+public class InvalidAgeException extends Exception{
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
